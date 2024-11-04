6cc91d7ddc67099bca3a2fa0c494d1ae44600389
Rosy122333 <80847580+Rosy122333@users.noreply.github.com>
2024-11-04 15:22:06 -0800

    only changing calc.py

--- calc.py
+++ calc.py
@@ -1,6 +1,7 @@
 # Basic Python Script: Simple Calculator
 def main():
-    #testing this file
+
+    #only changing this file
     # Taking input from the user
     num1 = float(input("Enter the first number: "))
     num2 = float(input("Enter the second number: "))
